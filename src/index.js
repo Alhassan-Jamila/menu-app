a90f258e45361cb0376b395f1a7fb91197a06e40
Alhassan-Jamila <jamilaal865@gmail.com>
2022-04-28 14:57:56 +0000

    added somedata

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import "./bootstrap.min.css";
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from "./context/auth.context"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <AuthContextProvider>
+  <BrowserRouter>
     <App />
-    </BrowserRouter>
+      </BrowserRouter>
+      </AuthContextProvider>
   </React.StrictMode>
 );
 
